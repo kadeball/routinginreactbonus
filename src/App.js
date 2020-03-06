650d697d7e18f0d421a09d4fc18d16500619ec16
kadeball <kadeball@iu.edu>
2020-03-05 19:41:55 -0600

    bonus redo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Data from "./components/Data";
+import Cool from "./components/Cool";
+import Pink from "./components/Pink";
+import Purple from "./components/Purple";
+import Blue from "./components/Blue";
+import Datainfo from "./components/Datainfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Router>
+          <Nav/>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/data' component={Data} exact/>
+            <Route exact path='/Cool' component={Cool} exact/>
+              <Route path="/data/:id?/:name?/:age?/:color?" component={Datainfo}/>
+              <Route exact path='/Pink' component={Pink} exact/>
+              <Route exact path='/Purple' component={Purple} exact/>
+              <Route exact path='/Blue' component={Blue} exact/>
+
+
+
+
+
+          </Switch>
+        </Router>
+      </div>
   );
 }
 
